8172684c1f93bf32b46201c45f6741563e26516e
Carla <carla240406@edu.unipar.br>
2026-05-27 21:54:14 -0300

    implementação da listagem de tarefas

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,7 +1,44 @@
 export default function TaskList() {
-    return (
-        <div>
-            <h2>Minhas Tarefas</h2>
+
+  const tarefas = [
+    {
+      titulo: 'Estudar JavaScript',
+      descricao: 'Manipulação do DOM',
+      prioridade: 'Alta',
+      data: '20/05/2026'
+    },
+
+    {
+      titulo: 'Trabalho DevOps',
+      descricao: 'Apresentação do projeto',
+      prioridade: 'Média',
+      data: '28/05/2026'
+    }
+  ]
+
+  return (
+    <div className="card">
+
+      <h2>Minhas Tarefas</h2>
+
+      {tarefas.map((tarefa, index) => (
+
+        <div className="task" key={index}>
+
+          <div>
+            <h3>{tarefa.titulo}</h3>
+            <p>{tarefa.descricao}</p>
+          </div>
+
+          <div>
+            <span>{tarefa.prioridade}</span>
+            <p>{tarefa.data}</p>
+          </div>
+
         </div>
-    )
+
+      ))}
+
+    </div>
+  )
 }
