dfd415c4a89deebaca0e217e9525d6e0a5882a88
Carla <carla240406@edu.unipar.br>
2026-05-27 22:03:07 -0300

    criação do formulário de cadastro de tarefas

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,7 +1,27 @@
 export default function AddTask() {
   return (
-    <div>
-      Formulário
+    <div className="card">
+
+      <h2>Adicionar Nova Tarefa</h2>
+
+      <div className="form">
+
+        <input
+          type="text"
+          placeholder="Digite a atividade..."
+        />
+
+        <select>
+          <option>Prioridade</option>
+          <option>Alta</option>
+          <option>Média</option>
+          <option>Baixa</option>
+        </select>
+
+        <button>Adicionar</button>
+
+      </div>
+
     </div>
   )
 }
