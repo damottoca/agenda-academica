33ef32d4942a14a858a4fba5ef2adadb54046161
Carla <carla240406@edu.unipar.br>
2026-05-27 21:37:51 -0300

    implementação da sidebar de navegação

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,23 @@
+import {
+  FaClipboardList,
+  FaBook,
+  FaCalendar,
+  FaChartBar,
+  FaCog
+} from 'react-icons/fa'
+
 export default function Sidebar() {
-    return (
-        <aside>
-            <h2>UNIPAR</h2>
-        </aside>
-    )
+  return (
+    <aside className="sidebar">
+      <h2>UNIPAR</h2>
+
+      <ul>
+        <li><FaClipboardList /> Tarefas</li>
+        <li><FaBook /> Matérias</li>
+        <li><FaCalendar /> Calendário</li>
+        <li><FaChartBar /> Resumo</li>
+        <li><FaCog /> Configurações</li>
+      </ul>
+    </aside>
+  )
 }
