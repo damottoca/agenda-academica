73c1effffa88b02026b067af384f9a0b6edaf14d
Carla <carla240406@edu.unipar.br>
2026-05-27 22:05:28 -0300

    implementação dos cards estatísticos

--- src/components/StatsCards.jsx
+++ src/components/StatsCards.jsx
@@ -1,7 +1,22 @@
 export default function StatsCards() {
   return (
-    <div>
-      Cards
+    <div className="stats">
+
+      <div className="stat-card">
+        <h3>Total</h3>
+        <p>4</p>
+      </div>
+
+      <div className="stat-card">
+        <h3>Pendentes</h3>
+        <p>3</p>
+      </div>
+
+      <div className="stat-card">
+        <h3>Concluídas</h3>
+        <p>1</p>
+      </div>
+
     </div>
   )
 }
