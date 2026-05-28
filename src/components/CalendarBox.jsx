ea6482dfce237a57adb816c300695c6f5a2b9f31
Carla <carla240406@edu.unipar.br>
2026-05-27 21:57:03 -0300

    adição do calendário acadêmico

--- src/components/CalendarBox.jsx
+++ src/components/CalendarBox.jsx
@@ -1,7 +1,10 @@
+import Calendar from 'react-calendar'
+import 'react-calendar/dist/Calendar.css'
+
 export default function CalendarBox() {
   return (
-    <div>
-      Calendário
+    <div className="card">
+      <Calendar />
     </div>
   )
 }
